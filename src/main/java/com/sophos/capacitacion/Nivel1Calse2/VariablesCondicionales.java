6b5522c97f3ed30abccfa6cff38f705857c6369a
aisavivi01 <avdn30@hotmail.com>
2020-07-17 18:35:20 -0500

    segundo commit

---
+++ src/main/java/com/sophos/capacitacion/Nivel1Calse2/VariablesCondicionales.java
@@ -0,0 +1,26 @@
+package com.sophos.capacitacion.Nivel1Calse2;
+
+public class VariablesCondicionales {
+	public static void main(String[] args) {
+      int edad1 = 46;
+      int edad2 = 17;
+      byte variableByte = 65;
+      byte variableByte2 = 58;
+      short variableShort = 32000;
+      short variableShort2 = 15000;
+      long variableLong = 55555;
+      long varaibleLong2 = 88888;
+      float variableFloat = (float) -5.555555;
+      float varaibleFloat2 = (float) 3.333333333333;
+      boolean variableBoolean = true;
+      boolean VariableBoolean2 = false;
+      char variableChar = 'A';
+      char variableChar2 = 'Z';
+            
+      if (edad1 >= 18) {
+          System.out.println("es mayor de edad");
+      }else if (edad1 < 18) {
+          System.out.println("es menor de edad");	  
+      }  
+  }
+ }
